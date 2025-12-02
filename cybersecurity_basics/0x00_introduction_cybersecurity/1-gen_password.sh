5c28057df2d6e2daca40d349df0d7302cd189195
TonNom <ton-email@example.com>
2025-12-02 15:40:29 +0100

    validate integr file

--- cybersecurity_basics/0x00_introduction_cybersecurity/1-gen_password.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/1-gen_password.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tr -dc '[:alnum:]' < /dev/urandom | head -c "${1:-32}"; echo
+LC_ALL=C tr -dc '[:alnum:]' < /dev/urandom | head -c "${1:-32}"; echo
---
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "$2  $1" | sha256sum -c - >/dev/null 2>&1 && echo "$1: OK" || echo "$1: FAILED"
