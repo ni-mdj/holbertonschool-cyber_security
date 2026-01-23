7a1d8c79a8e7897cff75b8e57ee8df2263b4a08e
TonNom <ton-email@example.com>
2026-01-23 10:48:27 +0100

    add new files

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -5,15 +5,15 @@ python3 - <<'PY' "$INPUT"
 from base64 import b64decode
 import sys
 
-Get argument and remove {xor} prefix
+# Get argument and remove {xor} prefix
 raw = sys.argv[1].replace("{xor}", "")
 
-Decode base64
+# Decode base64
 data = b64decode(raw)
 
-XOR with WebSphere legacy key (0x5f = '_')
+# XOR with WebSphere legacy key (0x5f = '_')
 decoded = bytes(byte ^ 0x5F for byte in data)
 
-Print result
+# Print result
 print(decoded.decode("utf-8"))
 PY
