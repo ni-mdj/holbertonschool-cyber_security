bfe115099b229812b8c145c80b9e806ca56da28d
TonNom <ton-email@example.com>
2026-04-10 15:53:44 +0200

    add all task

--- web_application_security/0x0c_web_application_foresics/0-service.sh
+++ web_application_security/0x0c_web_application_foresics/0-service.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "pam_unix" "auth.log" | sed -E 's/.pam_unix(([^)]+))./\1/' | cut -d ':' -f1 | sort | uniq -c | sort -nr
+grep "pam_unix" "auth.log" | sed -E 's/.*pam_unix\(([^)]+)\).*/\1/' | cut -d ':' -f1 | sort | uniq -c | sort -nr
